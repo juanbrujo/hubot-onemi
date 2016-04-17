439890a98933edec38f67838a62a34bfd3ba9681
Jorge Epuñan <juanbrujo@gmail.com>
2016-04-17 17:47:57 -0300

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
-# hubot-onemi
-Hubot script para las últimas alertas desde el sitio de Ministerio del Interior y Seguridad Pública de Chile (ONEMI)[onemi.cl]
+# Hubot Onemi
+
+**Hubot script para las últimas alertas desde el sitio de Ministerio del Interior y Seguridad Pública de Chile (ONEMI)[onemi.cl]**
+
+[![npm version](https://badge.fury.io/js/hubot-onemi.svg)](https://badge.fury.io/js/hubot-onemi)
+
+### Install:
+
+````
+$ npm install hubot-onemi --save
+````
+
+Add to `external-scripts.json`:
+
+````
+[
+  ..
+  "hubot-onemi"
+]
+````
+
+### Use:
+
+````
+hubot> hubot onemi <optional: rojo | verde | amarillo>
+````
+
+**Example:**
+
+````
+hubot> hubot onemi
+
+hubot>
+Vamos a onemi.cl a ver qué hay 🚌
+Se han encontrado *23 resultados* para _alerta _:
+1: Se actualiza Alerta Roja para Región Metropolitana por núcleo frío en altura | http://www.onemi.cl//alerta/declara-alerta-roja-para-comunas-de-la-rm-por-interrupcion-en-suministro-de-agua-potable
+2: Monitoreo por Núcleo Frío en Altura entre las regiones de Coquimbo y Aysén | http://www.onemi.cl//alerta/monitoreo-sistema-frontal-entre-las-regiones-de-coquimbo-y-aysen
+3: Interrupción suministro de electricidad en sectores de la Región de Valparaíso  | http://www.onemi.cl//alerta/interrupcion-suministro-de-electricidad-en-sectores-de-valparaiso
+> Otros resultados en: http://www.onemi.cl/alertas/     
+
+````
+
+### License:
+[MIT](https://opensource.org/licenses/MIT)
---
+++ hubot-onemi.js
@@ -0,0 +1,82 @@
+// Description:
+//   Hubot script para las últimas 5 alertas desde el sitio de Ministerio del Interior y Seguridad Pública de Chile (ONEMI)[onemi.cl]
+//
+//  Dependencies:
+//    cheerio
+//
+// Commands:
+//   hubot onemi <opcional tipo alerta: rojo | amarillo | verde>
+//
+// Author:
+//   @jorgeepunan
+
+var cheerio = require('cheerio');
+
+module.exports = function(robot) {
+  robot.respond(/onemi(.*)/i, function(msg) {
+
+    var baseURL = 'http://www.onemi.cl/';
+    var alertaURL = 'alertas/';
+    var alertaRojaURL = 'tipo/rojo/';
+    var alertaAmarillaURL = 'tipo/amarillo/';
+    var alertaVerdeURL = 'tipo/verde/';
+
+    var tipoAlerta = msg.match[1].split(' ')[1];
+    var nombreAlerta = '';
+
+    console.log(tipoAlerta);
+
+    if( tipoAlerta == 'rojo' || tipoAlerta == 'roja') {
+      alertaURL = alertaURL + alertaRojaURL;
+      nombreAlerta = 'rojo';
+    } else if( tipoAlerta === 'amarilla' || tipoAlerta === 'amarillo') {
+      alertaURL = alertaURL + alertaAmarillaURL;
+      nombreAlerta = 'amarillo';
+    } else if( tipoAlerta === 'verde') {
+      alertaURL = alertaURL + alertaVerdeURL;
+      nombreAlerta = 'verde';
+    } else {
+      nombreAlerta = 'general'
+      alertaURL = alertaURL;
+    }
+
+    msg.send('Vamos a onemi.cl a ver qué hay :bus: ');
+
+    msg.robot.http(baseURL + alertaURL).get()(function(err, res, body) {
+
+      var $ = cheerio.load(body);
+      var resultados = [];
+
+      $('.listado-alertas .row-fluid .span12').each(function() {
+        var title = $(this).find('.msg').text();
+        var link = $(this).find('> a').attr('href');
+        var fecha = $(this).find('.date').text(); // too much whitespace, must be improved
+
+        // resultados.push( title + '(' + fecha + ')' + ' | ' + baseURL + link );
+        resultados.push( title + ' | ' + baseURL + link );
+      });
+
+      if(resultados.length > 0) {
+
+        var limiteResultados = (resultados.length > 5) ? 3 : resultados.length;
+        var plural = resultados.length > 1 ? ['n','s'] : ['',''];
+
+        msg.send('Se ha' + plural[0] + ' encontrado *' + resultados.length + ' resultado' + plural[1] + '* para _alerta ' + nombreAlerta + '_:');
+
+        for (var i=0; i < limiteResultados; i++) {
+          var conteo = i + 1;
+          msg.send(conteo + ': ' + resultados[i]);
+        }
+
+        if(resultados.length > limiteResultados) {
+          msg.send('> Otros resultados en: '+ baseURL + alertaURL);
+        }
+
+      } else {
+        msg.send('No se han encontrado resultados. Intente nuevamente.');
+      }
+
+    });
+
+  });
+};
